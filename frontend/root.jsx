1f71968b7410d13d924820b2340acf8b2f035460
Andrew Schumacher <as228512@ohio.edu>
2018-05-08 12:17:11 -0400

    set up auth backend

---
+++ frontend/root.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-router-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root></Root>, root);
+});
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  context: __dirname,
+  entry: './frontend/root.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env', 'react']
+      }
+    },
+    {
+      test: /\.node$/,
+      loader: 'node-loader'
+    }
+    ]
+  }
+};
