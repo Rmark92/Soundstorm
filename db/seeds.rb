4e39dc839eac7f84766324c02759acee4d291230
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-05-09 14:42:45 -0400

    add demo_user to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-u1 = User.new({ username: 'Ryan', password: 'password'}).save!;
+u1 = User.new({ username: 'demo_user', password: 'password'}).save!;
