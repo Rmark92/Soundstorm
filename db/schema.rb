46e79a38ddbc202bd8c252f3a0c090f210253063
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-05-18 05:52:45 -0400

    add user timestamps in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515210522) do
+ActiveRecord::Schema.define(version: 20180518075234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20180515210522) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.integer "tracks_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
