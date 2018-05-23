e004187e290ec7c39e8aaa3aec0d2f19a0158e79
Ryan Mark <ryanmark2014@u.northwestern.edu>
2018-05-23 11:48:31 -0400

    remove logger from production, add favicon

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer.js';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 export const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
 };
